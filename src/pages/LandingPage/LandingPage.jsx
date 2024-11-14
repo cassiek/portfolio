42256b1aa98abed68b90d6239c542db24325e11a
cassiek <ckumpula123@gmail.com>
2024-11-14 12:12:18 -0500

    add files

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,15 @@
+import "./LandingPage.scss";
+import Header from "../../components/Header/Header.jsx";
+
+function LandingPage() {
+    return (
+        <>
+            <div>
+                <Header />
+                
+            </div>
+        </>
+    )
+};
+
+export default LandingPage;
