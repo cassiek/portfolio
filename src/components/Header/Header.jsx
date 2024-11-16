93656b2e658759cdec5231fa91dad359654ca2ce
cassiek <ckumpula123@gmail.com>
2024-11-16 00:02:51 -0500

    update header to add NavLink to LandingPage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,9 +6,12 @@ function Header() {
         <header className="header">
             <nav className="nav">
                 <ul className="nav__list">
-                    <li><NavLink to="/skills" className="nav__item">Skills</NavLink></li>
-                    <li><NavLink to="/projects" className="nav__item">Projects</NavLink></li>
-                    <li><NavLink to="/contact" className="nav__item">Contact</NavLink></li>
+                    <li><NavLink to="/" className="nav__landing">CK</NavLink></li>
+                    <div className="nav__menu">
+                        <li><NavLink to="/skills" className="nav__item">Skills</NavLink></li>
+                        <li><NavLink to="/projects" className="nav__item">Projects</NavLink></li>
+                        <li><NavLink to="/contact" className="nav__item">Contact</NavLink></li>
+                    </div>
                 </ul>
             </nav>
         </header>
