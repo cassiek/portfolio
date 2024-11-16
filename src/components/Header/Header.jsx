ef3e556f0b5993338e892f4c199457460a7e5baf
cassiek <ckumpula123@gmail.com>
2024-11-15 23:31:09 -0500

    fix NavLink styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
 import "./Header.scss";
-//import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Header() {
     return (
         <header className="header">
             <nav className="nav">
                 <ul className="nav__list">
-                    <li className="nav__item">Skills</li>
-                    <li className="nav__item">Projects</li>
-                    <li className="nav__item">Contact</li>
+                    <li><NavLink to="/skills" className="nav__item">Skills</NavLink></li>
+                    <li><NavLink to="/projects" className="nav__item">Projects</NavLink></li>
+                    <li><NavLink to="/contact" className="nav__item">Contact</NavLink></li>
                 </ul>
             </nav>
         </header>
