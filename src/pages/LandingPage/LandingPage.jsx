7a3b0a157001cd6d5a93434a130ca79e76ed1134
cassiek <ckumpula123@gmail.com>
2024-11-15 22:10:39 -0500

    add content to landing page, start styling

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -3,8 +3,15 @@ import "./LandingPage.scss";
 function LandingPage() {
     return (
         <>
-            <div>
-            </div>
+            <main className="landing-page">
+                <section className="landing-page__content">
+                    <h1 className="landing-page__header">
+                        Hi, I'm Cassie. 👋 <br className="landing-page__br--top"/>
+                        I'm a full-stack web developer. <br className="landing-page__br--bottom"/>
+                        Welcome to my portfolio.
+                    </h1>
+                </section>
+            </main>
         </>
     )
 };
