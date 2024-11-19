63119e460311497584db9062c57976745ea406f5
cassiek <ckumpula123@gmail.com>
2024-11-19 17:37:49 -0500

    style ResumePage

--- src/pages/ResumePage/ResumePage.jsx
+++ src/pages/ResumePage/ResumePage.jsx
@@ -10,15 +10,17 @@ import resume from "../../assets/resume/Cassie Kumpula Portfolio Resume.pdf";
 function ResumePage() {
     return (
         <main className="resume-page">
-            <h3 className="resume-page__header">Resume</h3>
-            <Document file={resume}>
-                <Page 
-                pageNumber={1}
-                renderTextLayer={false}
-                renderAnnotationLayer={false}
-                className="resume-page__page"
-                />
-            </Document>
+            <section className="resume-page__content">
+                <h3 className="resume-page__header">Resume</h3>
+                <Document file={resume}>
+                    <Page 
+                    pageNumber={1}
+                    renderTextLayer={false}
+                    renderAnnotationLayer={false}
+                    className="resume-page__page"
+                    />
+                </Document>
+            </section>
         </main>
     )
 };
