f24b58844f7d59127d763bf60e157efc7184e014
cassiek <ckumpula123@gmail.com>
2024-11-19 17:26:20 -0500

    work on styling ResumePage

--- src/pages/ResumePage/ResumePage.jsx
+++ src/pages/ResumePage/ResumePage.jsx
@@ -9,12 +9,14 @@ import resume from "../../assets/resume/Cassie Kumpula Portfolio Resume.pdf";
 
 function ResumePage() {
     return (
-        <main>
+        <main className="resume-page">
+            <h3 className="resume-page__header">Resume</h3>
             <Document file={resume}>
                 <Page 
                 pageNumber={1}
                 renderTextLayer={false}
                 renderAnnotationLayer={false}
+                className="resume-page__page"
                 />
             </Document>
         </main>
