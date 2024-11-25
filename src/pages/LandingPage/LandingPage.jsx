79bb6c40f3f2c07cbffabf88e0dbf34c67f5bb65
cassiek <ckumpula123@gmail.com>
2024-11-25 00:14:18 -0500

    install and implement react-type-animation for LandingPage header

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,28 +1,35 @@
 import "./LandingPage.scss";
-import { ReactTyped } from "react-typed";
+import Typewriter from "../../components/Typewriter/Typewriter.jsx";
 import Emoji from "../../components/Emoji/Emoji.jsx";
+import { TypeAnimation } from "react-type-animation";
 
 function LandingPage() {
 
     
     //<Emoji label="wave" symbol="👋" />
     //strings: ["Hi, I'm Cassie. 👋", "I'm a full-stack web developer.", "Welcome to my portfolio."],
-    
+    let wave = <Emoji label="wave" symbol="👋"></Emoji>
 
     return (
         <>
             <main className="landing-page">
                 <section className="landing-page__content">
 
-                    <ReactTyped 
-                        strings={["Hi, I'm Cassie.", "I'm a full-stack web developer.", "Welcome to my portfolio."]}
-                        typeSpeed={50}
-                        backSpeed={40}
-                        loop
-                        className="landing-page__header"
-                        showCursor={true}
-                    >
-                    </ReactTyped>
+                    <h1 className="landing-page__header"> 
+                        <TypeAnimation
+                            sequence={[
+                                "Hi, I'm Cassie. 👋",
+                                900,
+                                "I'm a full-stack web developer.",
+                                900,
+                                "Welcome to my portfolio.",
+                                900
+                            ]}
+                            wrapper="span"
+                            cursor={true}
+                            repeat={Infinity}
+                        />
+                    </h1>                            
                     
                 </section>
             </main>
