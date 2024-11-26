18daf3a2235e70e979651145464c2d2be3c429ff
cassiek <ckumpula123@gmail.com>
2024-11-26 06:33:18 -0500

    style LandingPage header

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,39 +1,30 @@
 import "./LandingPage.scss";
-import Typewriter from "../../components/Typewriter/Typewriter.jsx";
 import Emoji from "../../components/Emoji/Emoji.jsx";
 import { TypeAnimation } from "react-type-animation";
 
 function LandingPage() {
 
-    
-    //<Emoji label="wave" symbol="👋" />
-    //strings: ["Hi, I'm Cassie. 👋", "I'm a full-stack web developer.", "Welcome to my portfolio."],
-    let wave = <Emoji label="wave" symbol="👋"></Emoji>
-
     return (
-        <>
-            <main className="landing-page">
-                <section className="landing-page__content">
-
-                    <h1 className="landing-page__header"> 
-                        <TypeAnimation
-                            sequence={[
-                                "Hi, I'm Cassie. 👋",
-                                900,
-                                "I'm a full-stack web developer.",
-                                900,
-                                "Welcome to my portfolio.",
-                                900
-                            ]}
-                            wrapper="span"
-                            cursor={true}
-                            repeat={Infinity}
-                        />
-                    </h1>                            
-                    
-                </section>
-            </main>
-        </>
+        <main className="landing-page">
+            <section className="landing-page__content">
+                <h1 className="landing-page__header"> 
+                    <TypeAnimation
+                        sequence={[
+                            "Hi, I'm Cassie. 👋",
+                            900,
+                            "I'm a full-stack web developer.",
+                            900,
+                            "Welcome to my portfolio.",
+                            900
+                        ]}
+                        wrapper="span"
+                        cursor={true}
+                        repeat={Infinity}
+                    />
+                </h1>                            
+                
+            </section>
+        </main>
     )
 };
 
