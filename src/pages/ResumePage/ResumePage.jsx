fa9c8a1206b7013f9bc0a4824f25195abdfd377d
cassiek <ckumpula123@gmail.com>
2024-11-27 17:41:06 -0500

    update ResumePage to use <object> instead of imported library to display pdf

--- src/pages/ResumePage/ResumePage.jsx
+++ src/pages/ResumePage/ResumePage.jsx
@@ -1,26 +1,13 @@
 import "./ResumePage.scss";
-import { Document, Page, pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url,
-  ).toString();
 import resume from "../../assets/resume/Cassie Kumpula Portfolio Resume.pdf";
 
-
+// <object /> works in Firefox, Safari, Chrome
 function ResumePage() {
     return (
         <main className="resume-page">
             <section className="resume-page__content">
                 <h3 className="resume-page__header">Resume</h3>
-                <div className="resume-page__pdf">
-                    <Document file={resume} >
-                        <Page 
-                        pageNumber={1}
-                        renderTextLayer={false}
-                        renderAnnotationLayer={false}
-                        />
-                    </Document>
-                </div>
+                <object data={resume} type="application/pdf" className="resume-page__pdf"></object>    
             </section>
         </main>
     )
