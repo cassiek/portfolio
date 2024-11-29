8e3c7c302c1e4e34efa9cf4f76e9a94c67540ebb
cassiek <ckumpula123@gmail.com>
2024-11-29 13:44:09 -0500

    start building and styling Project component

---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,31 @@
+import "./Project.scss";
+import { Link } from "react-router";
+
+function Project({ title, stack, also, video, link, GitHub, description }) {
+    // preview to play on hover over rectangle
+    // link to GitHub or specify private repo
+
+  return (
+    <div className="project">
+        <h3 className="project__title">{title}</h3>
+        <p className="project__stack">Tech stack:{stack}</p>
+        <p className="project__also">Also:{also}</p>
+        <div className="project__preview">
+            <video controls>
+                <source src={video} type="" />
+            </video>
+        </div>
+        <div className="project__buttons">
+            <a href="#" className="project__link">{link}</a>
+            <Link to="#" className="project__video">{video}</Link>
+            <a href="#" className="project__code">{GitHub}</a>
+        </div>
+        <h4 className="project__overview">Overview</h4>
+        <p className="project__description">{description}</p>
+        {/* expandable section */}
+        {/* expandable section */}
+    </div>
+  )
+};
+
+export default Project;
---
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -0,0 +1,14 @@
+import "./ProjectsPage.scss";
+
+function ProjectsPage() {
+  return (
+    <main className="projects-page">
+        <section className="projects-page__content">
+            <h3 className="projects-page__header">Projects</h3>
+            
+        </section>
+    </main>
+  )
+};
+
+export default ProjectsPage;
