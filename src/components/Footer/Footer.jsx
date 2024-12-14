559d8111396801ffb137a2a54268a276f8b44e2b
cassiek <ckumpula123@gmail.com>
2024-12-13 22:47:24 -0500

    update footer to include links to linkedin, github, my email

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,16 @@
 import "./Footer.scss";
+import { Link } from "react-router-dom";
+import LinkedIn from "../../assets/icons/linkedin.png";
+import GitHub from "../../assets/icons/github.png";
+import email from "../../assets/icons/email.png";
+
 
 function Footer() {
     return (
         <footer className="footer">
+            <Link to="https://www.linkedin.com/in/cassie-k/"><img src={LinkedIn} className="footer__linkedin-icon" alt="LinkedIn icon linking to LinkedIn profile"/></Link>
+            <Link to="https://github.com/cassiek"><img src={GitHub} className="footer__github-icon" alt="GitHub icon linking to GitHub profile"/></Link>
+            <Link to="mailto:ckumpula123@gmail.com"><img className="footer__email-icon" src={email}/></Link>
             <p className="footer__copyright">
                 Cassandra Kumpula © 2024
             </p>
