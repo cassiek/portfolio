6d73b4ea6aff0f4bd5a19144b7d73af8e3f7cbeb
cassiek <ckumpula123@gmail.com>
2024-12-17 18:54:09 -0500

    solve icon animation issue by implementing background-image to Link instead of using img element

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,23 @@
 import "./Footer.scss";
 import { Link } from "react-router-dom";
 import LinkedIn from "../../assets/icons/linkedin.png";
+import darkLinkedIn from "../../assets/icons/linkedin-drkpurple.png";
 import GitHub from "../../assets/icons/github.png";
+import darkGitHub from "../../assets/icons/github-drkpurple.png";
 import email from "../../assets/icons/email.png";
+import darkEmail from "../../assets/icons/email-drkpurple.png";
 
 
 function Footer() {
+
+    //<img src={LinkedIn} className="footer__linkedin-icon" alt="LinkedIn icon linking to LinkedIn profile"/>
+    //<img src={GitHub} className="footer__github-icon" alt="GitHub icon linking to GitHub profile"/>
+    //<img className="footer__email-icon" src={email}/>
     return (
         <footer className="footer">
-            <Link to="https://www.linkedin.com/in/cassie-k/"><img src={LinkedIn} className="footer__linkedin-icon" alt="LinkedIn icon linking to LinkedIn profile"/></Link>
-            <Link to="https://github.com/cassiek"><img src={GitHub} className="footer__github-icon" alt="GitHub icon linking to GitHub profile"/></Link>
-            <Link to="mailto:ckumpula123@gmail.com"><img className="footer__email-icon" src={email}/></Link>
+            <Link to="https://www.linkedin.com/in/cassie-k/" className="linkedin"></Link>
+            <Link to="https://github.com/cassiek" className="github"></Link>
+            <Link to="mailto:ckumpula123@gmail.com" className="email"></Link>
             <p className="footer__copyright">
                 Cassandra Kumpula © 2024
             </p>
